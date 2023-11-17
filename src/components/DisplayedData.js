b8f85ffeb5323138996118482c74f771206fe845
Harsh <harshrastogi396@gmail.com>
2023-11-17 11:17:30 +0530

    edit functionality and save in local storage

--- src/components/DisplayedData.js
+++ src/components/DisplayedData.js
@@ -1,7 +1,19 @@
-import { Box, Button, Checkbox, Stack, Typography } from "@mui/material";
+import { Box, Button, Checkbox,TextField, Stack, Typography } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { useContext,useState } from "react";
+import { store } from "./TextArea";
+
+// import { useState } from "react";
 const DisplayedData = (props) => {
+  const[edit,setEdit]=useState(false)
+  const[updateInput,setUpdateInput]=useState('')
+  const allData = useContext(store);
+  const  handleEdit=()=>{
+    setEdit(!edit)
+    
+  }
   return (
     <>
       <Stack
@@ -12,6 +24,7 @@ const DisplayedData = (props) => {
           margin: "10px 0",
           padding: "0 20px",
           background: "white",
+          minHeight: "19%",
           boxShadow:
             "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
         }}
@@ -26,13 +39,16 @@ const DisplayedData = (props) => {
               sm: "space-between",
             },
             padding: "10px 10px",
+            alignItems: "center",
           }}
         >
           <Box>
             <Checkbox onClick={props.handleCheck} />
           </Box>
           <Box sx={{ wordBreak: "break-word" }}>
-            <Typography variant="h5">{props.input}</Typography>
+            {!edit?<Typography variant="h5">{props.text}</Typography>:<TextField 
+          label="Enter text" value={updateInput} onChange={(e)=>setUpdateInput(e.target.value)}/>}
+            {/* <Typography variant="h5">{props.text}</Typography> */}
           </Box>
         </Box>
         <Stack
@@ -47,13 +63,11 @@ const DisplayedData = (props) => {
               md: "10px",
             },
             wordBreak: "break-word",
-            // width:'100%'
           }}
         >
-         <Typography
-            // key={e.id}
+          <Typography
             variant="p"
-            color={'error'}
+            color={"error"}
             sx={{
               textAlign: "center",
               display: "flex",
@@ -61,14 +75,39 @@ const DisplayedData = (props) => {
             }}
           >
             {props.complete}
-          </Typography> 
-          <Button
+          </Typography>
+          {!edit ? (
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{ margin: "0px 10px" }}
+              onClick={handleEdit}
+            >
+              <EditIcon />
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ margin: "0px 10px" }}
+              onClick={()=>{
+                allData.saveEdit(props.data, updateInput)
+                setEdit(false)
+              }}
+
+            >
+              <AddIcon />
+            </Button>
+          )}
+          {/* <Button
             variant="contained"
             color="secondary"
             sx={{ margin: "0px 10px" }}
+            onClick={props.handleEdit}
           >
-            <EditIcon />
-          </Button>
+            {props.editIcon}
+          </Button> */}
+
           <Button variant="contained" color="error" onClick={props.deleteData}>
             <DeleteIcon />
           </Button>
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,14 +1,47 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Box, Button, Stack } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import DisplayedData from "./DisplayedData";
 import { v4 as uuidv4 } from "uuid";
+// import EditIcon from "@mui/icons-material/Edit";
 const store = createContext();
 const TextArea = (props) => {
   const [input, setInput] = useState("");
   const [data, setData] = useState([]);
-  // const [isChecked, setIsChecked] = useState(false);
+  // const[updateInput,setUpdateInput]=useState()
+
+  // useEffect(() => {
+  //   const storage = JSON.parse(localStorage.getItem("Data"));
+  //   setData(storage);
+  // }, []);
+  useEffect(() => {
+    // Retrieve data from local storage
+    const storedData = localStorage.getItem("Data");
+  
+    // Check if the retrieved data is not null or undefined
+    if (storedData) {
+      try {
+        // Parse the JSON data
+        const parsedData = JSON.parse(storedData);
+  
+        // Check if the parsed data is different from the current state
+        if (JSON.stringify(parsedData) !== JSON.stringify(data)) {
+          // Update the state with the parsed data
+          setData(parsedData);
+        }
+      } catch (error) {
+        console.error("Error parsing JSON data:", error);
+      }
+    }
+  },[]);
+  useEffect(() => {
+    if (data.length > 0) {
+      // Convert 'data' to a JSON string and store it in local storage
+      localStorage.setItem("Data", JSON.stringify(data));
+    }
+  }, [data]);
+
   const addData = () => {
     if (input.trim() !== "") {
       setData([...data, { id: uuidv4(), text: input, check: false }]);
@@ -25,19 +58,40 @@ const TextArea = (props) => {
 
   const clearAllText = () => {
     setData([]);
+    localStorage.clear();
   };
-  const handleCheck = (index) => {
-    setData(
-      data.map((e) => {
-        if (e.id === index) {
-          return { ...e, check: !e.check };
-        }
-          return e;
-      })
-    );
+  const handleCheck = (id) => {
+    const checkBoxData = data.map((e) => {
+      if (e.id === id) {
+        return { ...e, check: !e.check };
+      }
+      return e;
+    });
+    setData(checkBoxData);
   };
+  // saveEdit(id, updatedvalue){
+  //setData(()=>data.map((e)=>{if(e.id===id){return {...e, text:updatedvalue}}else {return e;}}))
+  // }
+  // localStorage.setItem('key', 'vlaue')
+  // data = [{},{id:2, text:param},{}]
+  const saveEdit = (id, updatedvalue) => {
+    if (updatedvalue.trim() !== "") {
+      setData(() =>
+        data.map((e) => {
+          if (e.id === id) {
+            return { ...e, text: updatedvalue };
+          } else {
+            return e;
+          }
+        })
+      );
+    } else {
+      alert("Please enter a valid value");
+    }
+  };
+
   return (
-    <store.Provider value={{ data }}>
+    <store.Provider value={{ data, input, setInput, saveEdit }}>
       <Stack
         direction={"row"}
         spacing={2}
@@ -61,18 +115,19 @@ const TextArea = (props) => {
           overflowX: "hidden",
           overflowY: "scroll",
           marginTop: "10px",
-          boxShadow: 'rgb(38, 57, 77) 0px 20px 30px -10px'
+          boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",
         }}
       >
         {data.map((e) => {
           return (
             <DisplayedData
-              input={e.text}
+              text={e.text}
               key={e.id}
               deleteData={() => deleteData(e.id)}
-              data={data}
+              data={e.id}
               handleCheck={() => handleCheck(e.id)}
-              complete={e.check&&'Completed'}
+              complete={e.check && "Completed"}
+              // editIcon={e.editIcon}
             />
           );
         })}
